bd1111c6afe9f606fdc9064dbd21f3178123ea83
breyes4 <71798440+breyes4@users.noreply.github.com>
2021-02-14 23:03:13 -0600

    Create bmi.js

---
+++ bmi.js
@@ -0,0 +1,69 @@
+$(document).ready(function () {
+
+    $("form").validate(
+        {
+            submitHandler: bmiOutput,
+            rules: bmiRules,
+            messages: bmiMessages,
+        }
+    );
+
+
+    var bmiRules =
+        {
+            height:
+                {
+                    required: true,
+                    min: 59,
+                    max: 79,
+                    digits: true
+                },
+
+            weight:
+                {
+                    required: true,
+                    min: 88,
+                    max: 353,
+                    digits: true
+                }
+        }
+
+    var bmiMessages =
+        {
+            height: {
+                required: "Height is required.",
+                min: "Minimum height is 59.",
+                max: "Maximum height is 73.",
+                digits: "Height must be a number."
+            },
+            weight: {
+                required: "Weight is required.",
+                min: "Minimum weight is 88.",
+                max: "Maximum weight is 353.",
+                digits: "Weight must be a number."
+            }
+        }
+
+
+
+    function bmiOutput() {
+        $("form").submit(bmiOutput);
+        var height = $("input[name=height]").val();
+        var weight = $("input[name=weight]").val();
+        var bmi = ((weight / (height * height)) * 703);
+
+        if (bmi < 18.5){
+            $("#message").text("Your BMI is " + bmi.toFixed(2) + " and you are underweight.");
+        }
+        else if (bmi >=18.5 && bmi < 25)
+        {
+            $("#message").text("Your BMI is " + bmi.toFixed(2) + " and you are healthy weight.");
+        }
+        else if (bmi >= 25 && bmi < 30) {
+            $("#message").text("Your BMI is " + bmi.toFixed(2) + " and you are overweight.");
+        }
+        else {
+            $("#message").text("Your BMI is " + bmi.toFixed(2) + " and you are obese.");
+        }
+    }
+});
